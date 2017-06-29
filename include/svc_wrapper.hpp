b100af1f612235d7fed3378fa4f85887911ca9ff
Subv <subv2112@gmail.com>
2017-06-29 11:23:59 -0500

    Expose more svcs to lua, namely:
    svcCreateEvent
    svcSignalEvent
    svcClearEvent
    svcCloseHandle

---
+++ include/svc_wrapper.hpp
@@ -0,0 +1,68 @@
+// The MIT License (MIT) 
+
+// Copyright (c) 2017 Subv and contributors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+#include <functional>
+#include <inttypes.h>
+
+namespace detail {
+template<typename RetT, typename Array, std::size_t... Indices>
+auto TupleFromArrayImpl(RetT ret, Array& arr, std::index_sequence<Indices...>) {
+    return std::make_tuple(ret, arr[Indices]...);
+}
+}
+
+template<typename RetT, typename T, std::size_t N, typename Indices = std::make_index_sequence<N>>
+auto TupleFromArray(RetT ret, std::array<T, N>& arr) {
+    return detail::TupleFromArrayImpl(ret, arr, Indices{});
+}
+
+template< typename... InArgs>
+struct ArrayApplier {
+    template<typename Func, typename T, std::size_t N, typename Indices = std::make_index_sequence<N>>
+    auto Apply(Func func, std::array<T, N>& arr, InArgs... in_args) {
+        return ApplyImpl(func, arr, Indices{}, in_args...);
+    }
+
+private:
+    template<typename Func, typename Array, std::size_t... Indices>
+    auto ApplyImpl(Func func, Array& arr, std::index_sequence<Indices...>, InArgs... in_args) {
+        return func(arr[Indices]..., in_args...);
+    }
+};
+
+template<typename Func, int NumOutArgs, typename... InArgs>
+auto Wrap(Func fun) {
+    return [=](InArgs... in_args) {
+        std::array<uint32_t, NumOutArgs> out_args;
+        std::array<uint32_t*, NumOutArgs> out_args_addr;
+
+        for (int i = 0; i < NumOutArgs; ++i) {
+            out_args_addr[i] = &out_args[i];
+        }
+
+        ArrayApplier<InArgs...> applier;
+
+        auto ret = applier.Apply(fun, out_args_addr, in_args...);
+        return TupleFromArray(ret, out_args);
+    };
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,42 @@
+// The MIT License (MIT) 
+
+// Copyright (c) 2017 Subv and contributors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #include <3ds.h>
-#include <string.h>
-#include <stdio.h>
 #include <sol.hpp>
+#include <stdio.h>
+#include "svc_wrapper.hpp"
 
 /// Registers the 3DS svc functions with the lua runtime
 void RegisterSVCs(sol::table& table) {
-    #define REG_SVC(name) table[#name] = svc##name
-    REG_SVC(OutputDebugString);
+    #define REG_SVC_SIMPLE(name) table[#name] = svc##name
+    #define REG_SVC(name, ...) table[#name] = Wrap<decltype(svc##name), __VA_ARGS__>(svc##name)
+    
+    REG_SVC(CreateEvent, 1, ResetType);
+    REG_SVC_SIMPLE(SignalEvent);
+    REG_SVC_SIMPLE(ClearEvent);
+    REG_SVC_SIMPLE(CloseHandle);
+    REG_SVC_SIMPLE(OutputDebugString);
+
     #undef REG_SVC
+    #undef REG_SVC_SIMPLE
 }
 
 int main() {
@@ -20,13 +49,16 @@ int main() {
 
     sol::state lua;
     // open some common libraries
-    lua.open_libraries(sol::lib::base, sol::lib::package);
+    lua.open_libraries(sol::lib::base);
 
     try {
         sol::table svc_table = lua.create_named_table("svc");
         RegisterSVCs(svc_table);
 
-        lua.script("svc.OutputDebugString('Hello', 5)", sol::default_on_error);
+        lua.script(R"_(svc.OutputDebugString('Hello', 5) 
+                       result, event = svc.CreateEvent(0)
+                       print('Result: ' .. result .. ' Event: ' .. event))_", 
+                    sol::default_on_error);
     } catch (const sol::error& err) {
         printf("Exception was thrown: %s\n", err.what());
     }
